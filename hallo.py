7b5f993d460ed44160cb333c9a8f7ded5ab8a795
Rob Koster <robkoster2907@gmail.com>
2021-09-06 12:09:00 +0200

    heb een python file toegevoegt

---
+++ hallo.py
@@ -0,0 +1,13 @@
+# hallo-python
+# Rob koster 
+#99067170
+
+print("Hello wereld van…    ")
+
+print("pppp    y     y   ttttttt   h   h    oooo   nnnn  ")
+print("p   p    y   y       t      h   h   o    o  n   n")
+print("p   p     y y        t      h   h   o    o  n   n")
+print("pppp       y         t      hhhhh   o    o  n   n")
+print("p          y         t      h   h   o    o  n   n")
+print("p          y         t      h   h   o    o  n   n")
+print("p          y         t      h   h    oooo   n   n")
